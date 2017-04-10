c379fcd266cda62d856dcbb233e17139a6e970bd
raccy <open@raccy.org>
2017-04-10 21:08:26 +0900

    GUIのログ削除を今度こそ削除

--- nicolive-log.h
+++ nicolive-log.h
@@ -6,6 +6,7 @@
 #ifndef __func__
 #define __func__ __FUNCTION__
 #endif
+
 #define nicolive_log_error(format, ...) \
 	blog(LOG_ERROR, "[nicolive] " format, ##__VA_ARGS__)
 #define nicolive_log_warn(format, ...) \
@@ -21,23 +22,4 @@
 #else
 #define nicolive_log_debug(format, ...)
 #define nicolive_log_debug_call_func()
-#endif
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void nicolive_mbox_error(const char *message);
-void nicolive_mbox_warn(const char *message);
-void nicolive_mbox_info(const char *message);
-
-void nicolive_msg_error(
-    bool available_gui, const char *gui_message, const char *cui_message);
-void nicolive_msg_warn(
-    bool available_gui, const char *gui_message, const char *cui_message);
-void nicolive_msg_info(
-    bool available_gui, const char *gui_message, const char *cui_message);
-
-#ifdef __cplusplus
-}
 #endif
